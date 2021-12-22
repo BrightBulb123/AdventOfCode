481dc835117ebe63225d171eb07ff2d01696a626
BrightBulb123 <uday.tur@gmail.com>
2021-12-22 12:44:48 +1100

    Solved Day5 Problem #2

--- Day5/Problem#2.py
+++ Day5/Problem#2.py
@@ -1,4 +1,4 @@
-file_name = "Day5/Day5TestInput.txt"
+file_name = "Day5/Day5Input.txt"
 
 with open(file_name) as file:
     co_ords = []
@@ -6,7 +6,7 @@ with open(file_name) as file:
         line = line.strip().split(" -> ")
         first_pair = tuple(int(i) for i in line[0].split(","))
         second_pair = tuple(int(i) for i in line[1].split(","))
-        co_ords.append(sorted((first_pair, second_pair)))
+        co_ords.append((first_pair, second_pair))
 
     def max_finder(co_ordinates: list, var: str) -> int:
         temp = []
@@ -24,36 +24,35 @@ with open(file_name) as file:
     table = [("".join("." for _ in range(max_y + 1))) for _ in range(max_x + 1)]
 
     for co_ords_pair in co_ords:
-        y_co_ords = tuple(i[1] for i in co_ords_pair)
-        x_co_ords = tuple(i[0] for i in co_ords_pair)
+        x1, x2, y1, y2 = co_ords_pair[0][0], co_ords_pair[1][0], co_ords_pair[0][1], co_ords_pair[1][1]
+        x_step = -1 if (x1 > x2) else 1
+        y_step = -1 if (y1 > y2) else 1
+        x_co_ords = [x for x in range(x1, x2+(x_step), x_step)]
+        y_co_ords = [y for y in range(y1, y2+(y_step), y_step)]
 
-        x1, x2 = x_co_ords
-        y1, y2 = y_co_ords
+        if len(x_co_ords) > len(y_co_ords):
+            y_co_ords *= len(x_co_ords)
+        elif len(x_co_ords) < len(y_co_ords):
+            x_co_ords *= len(y_co_ords)
 
-        if (not x1 == x2) and (not y1 == y2):
-            continue
+        for x, y in zip(x_co_ords, y_co_ords):
+            column = list(table[x])
+            try:
+                column[y] = int(column[y])
+                column[y] += 1
+            except ValueError:
+                column[y] = '1'
 
-        y_co_ords = tuple(sorted(i[1] for i in co_ords_pair))
-        x_co_ords = tuple(sorted(i[0] for i in co_ords_pair))
+            column = "".join(str(i) for i in column)
+            table[x] = column
 
-        for x in range(x_co_ords[0], x_co_ords[1] + 1):
-            row = table[x]
-            for y in range(y_co_ords[0], y_co_ords[1] + 1):
-                row = list(row)
-                try:
-                    row[y] = int(row[y])
-                    row[y] += 1
-                except ValueError:
-                    row[y] = 1
-                row = "".join(str(i) for i in row)
-                table[x] = row
-
-    table = [list(i) for i in zip(*table)]
+    table = ["".join(row) for row in (list(zip(*table)))]
 
     points = 0
     for row in table:
         for element in row:
             if (element != '.') and int(element) > 1:
                 points += 1
-        print(*row, sep='')
+
+    # print(*table, sep='\n')  # Uncomment this line to print out the entire diagram of the hydrothermal vent
     print(f"\nPoints: {points}")
