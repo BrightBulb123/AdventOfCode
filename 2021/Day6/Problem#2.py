341c13f89108f15cedf6c19d100f3c04a304aa46
BrightBulb123 <uday.tur@gmail.com>
2022-06-26 15:46:23 +1000

    Moved all into new 2021 folder + minor changes day 6

--- 2021/Day6/Problem#2.py
+++ 2021/Day6/Problem#2.py
@@ -5,7 +5,7 @@ with open(file_name) as file:
     line = file.readline().strip().split(',')
     all_fish = {t: line.count(str(t)) for t in range(9)}
 
-    for day in range(1, days + 1):
+    for _ in range(1, days + 1):
         new_fish = 0
         for timer, value in all_fish.items():
             if timer > 1:
@@ -25,7 +25,5 @@ with open(file_name) as file:
         if new_fish:
             all_fish[8] += new_fish
             all_fish[6] += new_fish
-
-        # print(f"Day {day} ({sum(all_fish.values())}): {all_fish}\n")  # Uncomment this line to see the fish populace changing each day
 
     print(f"\nAfter {days} days, the total number of lanternfish is: {sum(all_fish.values())}")
