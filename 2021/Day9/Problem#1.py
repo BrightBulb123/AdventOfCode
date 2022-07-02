dbc9fd1be77ee85e33c6e5e314173f3a160ee5ea
BrightBulb123 <83759628+BrightBulb123@users.noreply.github.com>
2022-07-02 21:22:57 +1000

    Day 9 - Part 1 - Works now

--- 2021/Day9/Problem#1.py
+++ 2021/Day9/Problem#1.py
@@ -10,7 +10,7 @@ lines = list(map(lambda x: x.replace('\n', ''), lines))
 def check_cell(depth: int, idx: int, lines: list[str]) -> bool:
     """Check up, down, left, and right of the current element at depth `depth` and index `idx`, and return True if it is the lowest value among them."""
 
-    up, down, left, right, num = 10, 10, 10, 10, int(lines[depth][idx])
+    up, down, left, right, num = None, None, None, None, int(lines[depth][idx])
     if depth != 0:
         up = int(lines[depth - 1][idx])
     if depth != len(lines) - 1:
@@ -21,23 +21,23 @@ def check_cell(depth: int, idx: int, lines: list[str]) -> bool:
     if idx != len(lines[depth]) - 1:
         right = int(lines[depth][idx + 1])
 
-    if up != None and up < num:
+    if up != None and up <= num:
         return False
 
-    if down != None and down < num:
+    if down != None and down <= num:
         return False
 
-    if left != None and left < num:
+    if left != None and left <= num:
         return False
 
-    if right != None and right < num:
+    if right != None and right <= num:
         return False
 
     return True
 
 
-final = []
+low_points = []
 for depth, row in enumerate(lines):
-    final.extend(int(char) for idx, char in enumerate(row) if check_cell(depth=depth, idx=idx, lines=lines))
+    low_points.extend(int(char) for idx, char in enumerate(row) if check_cell(depth=depth, idx=idx, lines=lines))
 
-print(sum(map(lambda x: x+1, final)))
+print(sum(map(lambda x: x+1, low_points)))
