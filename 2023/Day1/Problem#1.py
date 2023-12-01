4510042ae31d9a28e8801ec6681e5b40783d3b56
BrightBulb123 <uday.tur@gmail.com>
2023-12-01 22:42:55 +1100

    Completed 2023 Day 1

---
+++ 2023/Day1/Problem#1.py
@@ -0,0 +1,20 @@
+import re
+
+
+file_name = "2023/Day1/Day1Input.txt"
+
+
+with open(file_name) as file:
+    lines = map(lambda x: x.replace("\n", ""), file.readlines())
+
+digits_pattern = re.compile(r"\d")
+
+total = 0
+for line in lines:
+    matches = list(re.finditer(digits_pattern, line))
+    first, last = matches[0].group(0), matches[-1].group(0)
+    calibration_value = int(f"{first}{last}")
+    total += calibration_value
+
+
+print(total)
---
+++ 2023/Day1/Problem#2.py
@@ -0,0 +1,25 @@
+import regex as re
+
+
+file_name = "2023/Day1/Day1Input.txt"
+
+
+with open(file_name) as file:
+    lines = map(lambda x: x.replace("\n", ""), file.readlines())
+
+words_to_digits = {word:(digit+1) for digit, word in enumerate("one|two|three|four|five|six|seven|eight|nine|ten".split("|"))}
+digits_pattern = re.compile(r"\d|one|two|three|four|five|six|seven|eight|nine|ten")
+
+total = 0
+for line in lines:
+    matches = list(re.finditer(digits_pattern, line, overlapped=True))
+    first, last = matches[0].group(0), matches[-1].group(0)
+    if first.isalpha():
+        first = words_to_digits[first]
+    if last.isalpha():
+        last = words_to_digits[last]
+    calibration_value = int(f"{first}{last}")
+    total += calibration_value
+
+
+print(total)
