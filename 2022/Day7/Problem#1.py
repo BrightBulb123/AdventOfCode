81e11bdad9222c3d8a62f89e7c7d505ebabb8cad
BrightBulb123 <uday.tur@gmail.com>
2023-12-03 14:48:13 +1100

    Attempted rewrite of 2022 day 7, still not working

--- 2022/Day7/Problem#1.py
+++ 2022/Day7/Problem#1.py
@@ -1,91 +1,90 @@
-from __future__ import annotations
-from pprint import pprint
+import re
 
 
 file_name = "2022/Day7/Day7TestInput.txt"
 
-with open(file_name) as file:
-    lines = file.readlines()
 
-lines = list(map(lambda x: x.replace("\n", ""), lines))
+with open(file_name) as file:
+    lines = map(lambda x: x.replace("\n", ""), file.readlines())
+
+
+class Directory:
+    def __init__(self, name: str, contents=None, parent=None) -> None:
+        if contents is None:
+            contents = []
+        self.name = name
+        self.contents = contents
+        self.size = self.get_size()
+        self.parent = parent
+
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, Directory):
+            return NotImplemented
+
+        return self.name == __value.name
+
+    def __hash__(self) -> int:  # Because I implemented __eq__()
+        return hash(tuple(self.name))
+
+    def get_size(self) -> int:
+        return sum(item.size for item in self.contents)  # 0 if self.contents == []
+
+    def add_to_contents(self, item) -> None:
+        if isinstance(item, File):
+            if item not in self.contents:
+                self.contents.append(item)
+        else:
+            item.parent = self
+            if item in self.contents:
+                if item.size >= self.contents[self.contents.index(item)]:
+                    self.contents[self.contents.index(item)] = item
+            else:
+                self.contents.append(item)
+            self.size = self.get_size()
 
 
 class File:
-    def __init__(self, name: str, size: int) -> None:
+    def __init__(self, name: str, extension: str, size: int) -> None:
         self.name = name
+        self.extension = extension
         self.size = size
 
-    def __eq__(self, __o: object) -> bool:
-        if isinstance(__o, self.__class__):
-            return self.__dict__ == __o.__dict__
-        else:
-            return False
+    def __eq__(self, __value: object) -> bool:
+        if not isinstance(__value, File):
+            return NotImplemented
 
-    def __ne__(self, __o: object) -> bool:
-        return not self.__eq__(__o)
+        return (
+            self.name == __value.name
+            and self.extension == __value.extension
+            and self.size == __value.size
+        )
 
-    def __repr__(self) -> str:
-        return f"{self.name} - {self.size}"
+    def __hash__(self) -> int:  # Because I implemented __eq__()
+        return hash((self.name, self.size))
 
 
-directories: dict[tuple(str, int), list[tuple(str, int) | File]] = {}
-
-current_directory = None
-children = []
-level = 0
-
+root = Directory("~")
+current_dir = root  # Reference, not a copy, so any changes are still made to root
+list_mode = False
 for line in lines:
     if "$" in line:
+        list_mode = False
         if "cd" in line:
             if ".." in line:
-                level -= 1
+                current_dir = current_dir.parent
             else:
-                if current_directory is not None:
-                    directories[current_directory] = children
-                children = []
-
-                current_directory = (line.replace("$ cd ", ""), level)
-                directories[current_directory] = children
-                level += 1
-    elif "dir" in line:
-        directory = (line.replace("dir ", ""), level)
-        children.append(directory)
-    else:
-        file = File(line.split(" ")[-1], int(line.split(" ")[0]))
-        children.append(file)
-
-
-pprint(directories, compact=True)
-
-
-checked = {k:False for k in directories}
-sizes = {k:0 for k in directories}
-
-
-def directory_size_checker(directory: tuple(str, int)) -> int:
-    contents = directories[directory]
-    size = 0
-    if not checked[directory]:
-        for item in contents:
-            size += item.size if isinstance(item, File) else directory_size_checker(item)
-            sizes[directory] = size
-            checked[directory] = True
-    else:
-        size = sizes[directory]
-    return size
-
-
-total = 0
-for directory, contents in directories.items():
-    size = 0
-    if not checked[directory]:
-        size = directory_size_checker(directory=directory)
-        sizes[directory] = size
-        checked[directory] = True
-    else:
-        size = sizes[directory]
-    if size <= 100_000:
-        total += size
-
-
-print("\n\n" + str(total))
+                new_directory = Directory(line.split("$ cd ")[-1])
+                current_dir.add_to_contents(new_directory)
+                current_dir = new_directory
+        elif "ls" in line:
+            list_mode = True
+    elif list_mode:
+        if "dir" in line:
+            name = line.split("dir ")[-1]
+            new_directory = Directory(name=name)
+            current_dir.add_to_contents(new_directory)
+        else:
+            size = int(re.split(r"[^0-9]+", line)[0])
+            name, extension = re.split(r"\d+ ", line)[-1].split(".")
+            f = File(name=name, extension=extension, size=size)
+            current_dir.add_to_contents(f)
