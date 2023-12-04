d025c2112fe2c1517c7c892193907c7f28e6002e
BrightBulb123 <uday.tur@gmail.com>
2023-12-04 13:54:18 +1100

    Started Day 8 2022, test input works, real does not

---
+++ 2022/Day8/Problem#1.py
@@ -0,0 +1,50 @@
+file_name = "2022/Day8/Day8Input.txt"
+
+
+with open(file_name) as file:
+    lines = file.read().splitlines()
+
+
+lines = [[int(i) for i in line] for line in lines]
+
+FOREST_WIDTH = len(lines[0])
+visible_trees = []
+for line_num, line in enumerate(lines):
+    for column_num in range(FOREST_WIDTH):
+        co_ords = (column_num, line_num)  # (x,y)
+        number = line[column_num]
+
+        if line_num in [0, len(lines) - 1]:  # Top and bottom edges
+            visible_trees.append(co_ords)
+            continue
+        if column_num in [0, len(line) - 1]:  # Left and right edges
+            visible_trees.append(co_ords)
+            continue
+
+        numbers_before = line[:column_num]
+        numbers_after = line[column_num + 1 :]  # inclusive:exclusive
+        numbers_lower_before = [i if i < number else None for i in numbers_before]
+        numbers_lower_after = [i if i < number else None for i in numbers_after]
+        if (numbers_before[-1] < number and all(numbers_lower_before)) or (
+            (numbers_after[0] < number) and all(numbers_lower_after)
+        ):
+            visible_trees.append(co_ords)
+            continue
+
+        numbers_above = [l[column_num] for l in lines[:line_num]]
+        numbers_below = [
+            l[column_num] for l in lines[line_num + 1 :]
+        ]  # inclusive:exclusive
+        numbers_lower_above = [i if i < number else None for i in numbers_above]
+        numbers_lower_below = [i if i < number else None for i in numbers_below]
+        if (
+            (numbers_above[-1] < number)
+            and (all(numbers_lower_above))
+            or ((numbers_below[0] < number) and all(numbers_lower_below))
+        ):
+            visible_trees.append(
+                co_ords
+            )  # This one doesn't need a `continue` since it's at the end
+
+
+print(len(visible_trees))
